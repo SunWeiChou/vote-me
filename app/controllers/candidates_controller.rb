fec9e5745b045426f48aeacbae07d866bf53ebd8
SunWei Chou <sunwei8524@gmail.com>
2021-04-03 01:40:39 +0800

    將表單存入資料庫

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -3,8 +3,22 @@ class CandidatesController < ApplicationController
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
+    @candidate = Candidate.new(condidate_params)
+
+    if @candidate.save
+      flash[:notice] = "創建成功"
+      redirect_to '/candidates'
+    else
+      #NG
+    end
   end
+
+  def condidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
+
 end
