341290ff0ce5b6e267dd3fe84d3a702055b3f3d7
SunWei Chou <sunwei8524@gmail.com>
2021-04-05 00:43:26 +0800

    add 刪除功能

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -37,6 +37,13 @@ class CandidatesController < ApplicationController
     end
   end
 
+  def destroy
+    @candidate = Candidate.find_by(id: params[:id])
+    @candidate.destroy
+    flash[:notice] = "刪除成功"
+    redirect_to '/candidates'
+  end
+
 
   private
   def condidate_params
